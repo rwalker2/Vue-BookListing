6d449e5c1d329b4e16f7d5063440ce521e469877
Richard <r.walker2@ucas.ac.uk>
2018-06-29 16:05:26 +0100

    Lopping part, list created

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="books">
     <h1>{{ title }}</h1>
+    <ul>
+        <li>Book 1</li>
+        <li>Book 2</li>
+        <li>Book 3</li>
+    </ul>
   </div>
 </template>
 
@@ -19,5 +24,16 @@ export default {
 h1, h2 {
   font-weight: normal;
 }
+
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+    display: block;
+    margin: 0 10px;
+}
+
 </style>
 
